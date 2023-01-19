a9b6957d48c5406f40a16d374b165dd396dde6c2
vovkaWeb <vova77557@gmail.com>
2023-01-19 15:29:49 +0200

    +

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+import "./App.css";
+
+class Form extends Component {
+  //  componentDidMount(){
+  // 	this.myRef.current.focus();
+  //  }
+  setInputRef = (elem) => {
+    this.myRef = elem;
+  };
+  focusFirstTI = () => {
+	if(this.myRef)
+    this.myRef.focus();
+  };
+
+  render() {
+    return (
+      <Container>
+        <form className="w-50 border mt-5 p-3 m-auto">
+          <div className="mb-3">
+            <label
+              htmlFor="exampleFormControlInput1"
+              className="form-label">
+              Email address
+            </label>
+
+            <input
+              ref={this.setInputRef}
+              type="email"
+              className="form-control"
+              id="exampleFormControlInput1"
+              placeholder="name@example.com"
+            />
+          </div>
+          <div className="mb-3">
+            <label
+              htmlFor="exampleFormControlTextarea1"
+              className="form-label">
+              Example textarea
+            </label>
+            <textarea
+              onClick={this.focusFirstTI}
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"></textarea>
+          </div>
+        </form>
+      </Container>
+    );
+  }
+}
+class TextInput extends Component {
+  doSmth = () => {
+    console.log("kkk");
+  };
+  render() {
+    return (
+      <input
+        type="email"
+        className="form-control"
+        id="exampleFormControlInput1"
+        placeholder="name@example.com"
+      />
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Form />;
 }
 
 export default App;
